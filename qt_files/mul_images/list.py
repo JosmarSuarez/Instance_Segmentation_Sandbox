671a5a8c8110b72d67325605d26f9ba9952c296c
josmar <luisjosmar@hotmail.com>
2020-07-23 21:55:08 -0400

    Added list py that allows to watch displaced images

---
+++ qt_files/mul_images/list.py
@@ -0,0 +1,74 @@
+from list_ui import *
+import json
+import random
+import cv2
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+
+               
+        self.listWidget.clicked.connect(self.clicked)
+        global dic
+        dic, files = self.read_json('displaced.json')
+        
+
+        #lists all the files listed inside the dictionary
+        c=0
+        for f in files:
+            self.listWidget.insertItem(c, f)
+            c+=1
+        
+    
+    def clicked(self, qmodelindex):
+        item = self.listWidget.currentItem()
+        key = item.text()
+                
+        self.clearLayout(self.gridLayout_2)
+        sample = random.sample(dic[key], 9)
+        labels=[]
+        c=0
+
+        for img_name in sample:
+            i_label = QtWidgets.QLabel()
+            # i_label.setText(img_name)
+
+            sil_path = "../../datasets/casia_B1_silhouettes/{}-{}.png".format(key, img_name)
+            img_path = "../../datasets/casia_B1_images/{}-{}.jpg".format(key, img_name)
+            img = cv2.imread(img_path)
+            sil = cv2.imread(sil_path)
+
+            alpha = 0.4
+            beta = (1.0-alpha)
+            new_img = cv2.addWeighted(img, alpha, sil, beta, 0.0)
+            #cv2 image converted to qt image
+            qtimg = QtGui.QImage(new_img.data, img.shape[1], img.shape[0],QtGui.QImage.Format_RGB888)
+            pixmap = QtGui.QPixmap(QtGui.QPixmap.fromImage(qtimg))
+            i_label.setPixmap(pixmap)
+            labels.append(i_label)
+
+            self.gridLayout_2.addWidget(i_label, c//3, c%3, 1, 1)
+            c+=1
+
+
+        print(key, sample)
+    
+    def read_json(self,path):
+        with open(path) as f:
+            folders = json.load(f)
+            list_folders = list(folders.keys())
+            list_folders.sort() 
+        return  folders, list_folders
+
+    
+    def clearLayout(self,layout):
+        for i in reversed(range(layout.count())): 
+            layout.itemAt(i).widget().setParent(None)
+    
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
---
+++ qt_files/mul_images/list_ui.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'list.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName("gridLayout")
+        self.scrollArea = QtWidgets.QScrollArea(self.centralwidget)
+        self.scrollArea.setAutoFillBackground(False)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 518, 538))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.gridLayout_2 = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout_2.setObjectName("gridLayout_2")
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.gridLayout.addWidget(self.scrollArea, 1, 1, 1, 1)
+        self.listWidget = QtWidgets.QListWidget(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.listWidget.sizePolicy().hasHeightForWidth())
+        self.listWidget.setSizePolicy(sizePolicy)
+        self.listWidget.setObjectName("listWidget")
+        self.gridLayout.addWidget(self.listWidget, 1, 2, 1, 1)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 20))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
+
